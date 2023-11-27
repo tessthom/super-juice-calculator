e50a5dc4e84fddcf6a4b6e25c712586091d75f3f
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-11-27 15:32:20 -0500

    Finished script and styling for recipe generator.

--- script.js
+++ script.js
@@ -22,15 +22,34 @@ function validateInput(input) {
   if (isNaN(input) || input === '' || input < 1) {
     error.style.display = 'block';
     error.textContent = 'Please enter a valid number';
+    console.log('validateInput is about to return false');
     return false;
   } else {
     error.textContent = '';
+    console.log('input is valid and the validation function has run')
     return true;
   }
 }
 
 // Store input values in session storage -------------------------
-function insertResults(juiceAMount, peelWeight, totalWaterWeight, totalAcidPercentage, totalAcidWeight, citricAcid, malicAcid) {
+function insertResults(
+  fruit,
+  capFruit,
+  citricAcidPercent,
+  malicAcidPercent,
+  juiceAMount,
+  peelWeight,
+  totalWaterWeight,
+  totalAcidPercentage,
+  totalAcidWeight,
+  citricAcid,
+  malicAcid
+) {
+  // store values in session storage
+  sessionStorage.setItem('fruit', fruit);
+  sessionStorage.setItem('capFruit', capFruit);
+  sessionStorage.setItem('citricAcidPercent', citricAcidPercent);
+  sessionStorage.setItem('malicAcidPercent', malicAcidPercent);
   sessionStorage.setItem('juiceAMount', juiceAMount);
   sessionStorage.setItem('peelWeight', peelWeight);
   sessionStorage.setItem('totalWaterWeight', totalWaterWeight);
@@ -46,7 +65,6 @@ function insertResults(juiceAMount, peelWeight, totalWaterWeight, totalAcidPerce
 // Input type toggle -------------------------
 const toggleInputBtn = document.getElementById('toggleInputBtn');
 
-
 // function that changes the input label and name if input is toggled between final desired amount of juice or weight of peels when user clicks on the inputTypeBtn button
 function toggleInputType() {
   const inputLabel = document.querySelector('.inputTypeLabel');
@@ -61,76 +79,77 @@ function toggleInputType() {
 function calculateRecipe() {
   // validate input
   const inputValue = document.getElementById('inputValue').value;
-  if (!validateInput(inputValue)) { // if input invalid...
+  if (!validateInput(inputValue)) {
+    // if input invalid...
     return;
   }
-  // get values from select option and input field
-  const selectOption = document.getElementById('selectOption').value;
-  let juiceAMount;
-  let peelWeight;
+  // get values from select option
+  let fruit = document.getElementById('fruit').value;
+  let capFruit = fruit.charAt(0).toUpperCase() + fruit.slice(1);
+
+  // set each acid percentage based on fruit selected, to provide details for in recipe
+  let citricAcidPercent;
+  let malicAcidPercent;
+  if (fruit === 'lime') {
+    citricAcidPercent = 4.5;
+    malicAcidPercent = 1.5;
+  } else if (fruit === 'lemon') {
+    citricAcidPercent = 5;
+    malicAcidPercent = 1;
+  }
 
   // if input label is for juice, assign value to juiceAmount variable, else assign value to a peelWeight variable
-  if (document.querySelector('.inputTypeLabel').textContent === 'Amount of juice you want (ml):') {
-    juiceAMount = document.getElementById('input').value;
+  let juiceAmount;
+  let peelWeight;
+  let totalWaterWeight;
+  if (
+    document.querySelector('.inputTypeLabel').textContent ===
+    'Amount of juice you want (ml):'
+  ) {
+    juiceAmount = inputValue;
+    totalWaterWeight = juiceAmount;
+    peelWeight = totalWaterWeight / 10;
   } else {
-    peelWeight = document.getElementById('input').value;
+    peelWeight = inputValue;
+    totalWaterWeight = peelWeight * 10;
+    juiceAmount = totalWaterWeight;
   }
 
   // the output variables are: peelWeight, juiceAmount, totalWaterWeight, totalAcidPercentage (which is 6% for limes), totalAcidWeight citricAcid, malicAcid. The formula used if limes are the selected fruit option is: peelWeight * 10 = totalWaterWeight, totalWaterWeight * totalAcidPercentage = totalAcidWeight, citricAcid = totalAcidWeight * .45, malicAcid = totalAcidWeight * .15.
-  // instantiate all variables
-  let totalWaterWeight;
+  // instantiate remaining calculated variables
   let totalAcidPercentage;
   let totalAcidWeight;
   let citricAcid;
   let malicAcid;
 
   // if lime is selected, set totalAcidPercentage to 6%, citricAcid being 4.5% of totalAcidWeight, malicAcid being 1.5% of totalAcidWeight
-  if (selectOption === 'lime') {
-    totalAcidPercentage = .06;
-    totalWaterWeight = peelWeight * 10;
+  if (fruit === 'lime') {
+    totalAcidPercentage = 0.06;
     totalAcidWeight = totalWaterWeight * totalAcidPercentage;
-    citricAcid = totalAcidWeight * .45;
-    malicAcid = totalAcidWeight * .15;
-  } else if (selectOption === 'lemon') {
-    totalAcidPercentage = .05;
-    totalWaterWeight = peelWeight * 10;
+    citricAcid = (totalAcidWeight * 0.45).toFixed(1);
+    malicAcid = (totalAcidWeight * 0.15).toFixed(1);
+  } else if (fruit === 'lemon') {
+    totalAcidPercentage = 0.05;
     totalAcidWeight = totalWaterWeight * totalAcidPercentage;
-    citricAcid = totalAcidWeight * .45;
-    malicAcid = totalAcidWeight * .15;
-  } else if (selectOption === 'orange') {
-    totalAcidPercentage = .04;
-    totalWaterWeight = peelWeight * 10;
-    totalAcidWeight = totalWaterWeight * totalAcidPercentage;
-    citricAcid = totalAcidWeight * .45;
-    malicAcid = totalAcidWeight * .15;
-  } else if (selectOption === 'grapefruit') {
-    totalAcidPercentage = .03;
-    totalWaterWeight = peelWeight * 10;
-    totalAcidWeight = totalWaterWeight * totalAcidPercentage;
-    citricAcid = totalAcidWeight * .45;
-    malicAcid = totalAcidWeight * .15;
-  } else if (selectOption === 'pomelo') {
-    totalAcidPercentage = .02;
-    totalWaterWeight = peelWeight * 10;
-    totalAcidWeight = totalWaterWeight * totalAcidPercentage;
-    citricAcid = totalAcidWeight * .45;
-    malicAcid = totalAcidWeight * .15;
-  } else if (selectOption === 'kumquat') {
-    totalAcidPercentage = .07;
-    totalWaterWeight = peelWeight * 10;
-    totalAcidWeight = totalWaterWeight * totalAcidPercentage;
-    citricAcid = totalAcidWeight * .45;
-    malicAcid = totalAcidWeight * .15;
-  } else if (selectOption === 'yuzu') {
-    totalAcidPercentage = .08;
-    totalWaterWeight = peelWeight * 10;
-    totalAcidWeight = totalWaterWeight * total
-  };
-
+    citricAcid = (totalAcidWeight * 0.9).toFixed(1);
+    malicAcid = (totalAcidWeight * 0.1).toFixed(1);
+  }
+  console.log(`fruit specific calculation has been done and the variables are fruit: ${fruit}, juiceAmount: ${juiceAmount}, peelWeight: ${peelWeight}, citricAcidPercent: ${citricAcidPercent}, $malicAcidPercent: ${malicAcidPercent}, totalAcidPercentage: ${totalAcidPercentage}, totalAcidWeight: ${totalAcidWeight}, citricAcid: ${citricAcid}, malicAcid: ${malicAcid}.`);
+  //console.log(fruit, citricAcidPercent, malicAcidPercent, juiceAmount, peelWeight, totalWaterWeight, totalAcidPercentage, totalAcidWeight, citricAcid, malicAcid);
+  // format acid percentage for render
+  totalAcidPercentage = (totalAcidPercentage * 100).toFixed(1);
   // call fn to insert results into recipe.html DOM
-  insertResults(juiceAMount, peelWeight, totalWaterWeight, totalAcidPercentage, totalAcidWeight, citricAcid, malicAcid);
-
-  // open recipe.html using best practice - window.location?
-
+  insertResults(
+    fruit,
+    capFruit,
+    citricAcidPercent,
+    malicAcidPercent,
+    juiceAmount,
+    peelWeight,
+    totalWaterWeight,
+    totalAcidPercentage,
+    totalAcidWeight,
+    citricAcid,
+    malicAcid
+  );
 }
-
